ed77f14adad925c5803958b138466e0df0d8bc14
kojix2 <2xijok@gmail.com>
2023-03-27 23:18:07 +0900

    That wasn't my first commit earlier...

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# deepl
+# DeepL Translator CLI
+
+A command-line interface (CLI) tool for translating text using DeepL API.
+
+## Installation
+
+1. Clone this repository
+2. [Get a valid API key from DeepL](https://www.deepl.com/pro-api)
+
+## Usage
+
+```bash
+$ deepl [arguments]
+```
+
+### Arguments
+
+Options available for the CLI tool:
+
+- `-f, --from=LANG`: Set the source language (default: AUTO)
+- `-t, --to=LANG`: Set the target language (default: EN)
+- `-v, --version`: Show the current version
+- `-h, --help`: Show the help message
+
+### Example
+
+```bash
+$ deepl --from=ES --to=EN "Hola mundo"
+Hello world
+```
+
+## Contributing
+
+1. Fork this repository
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## License
+
+This project is licensed under the MIT License. 
--- src/deepl.cr
+++ src/deepl.cr
@@ -1,9 +1,39 @@
+require "option_parser"
 require "./deepl/translator"
+require "./deepl/version"
 
-input_text = ARGV[0]
 target_lang = "EN"
+source_lang = ""
+
+parser = OptionParser.new do |opts|
+  opts.banner = "Usage: deepl [arguments]"
+  opts.on("-f", "--from=LANG", "Source language [AUTO]") { |from| source_lang = from }
+  opts.on("-t", "--to=LANG", "Target language [EN]") { |to| target_lang = to }
+  opts.on("-v", "--version", "Show version") do
+    puts Deepl::VERSION
+    exit
+  end
+  opts.on("-h", "--help", "Show this help") do
+    puts opts
+    exit
+  end
+  opts.on_invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts opts
+    exit(1)
+  end
+end
+
+parser.parse(ARGV)
+
+input_text = ARGV.join(" ")
+
+if input_text.empty?
+  STDERR.puts parser
+  exit(1)
+end
 
 translator = Deepl::Translator.new
-translated_text = translator.translate(input_text, target_lang)
+translated_text = translator.translate(input_text, target_lang, source_lang)
 
 puts translated_text
