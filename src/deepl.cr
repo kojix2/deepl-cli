a19b2efa324b7dff5b7f57c5148a80f655c7a2b9
kojix2 <2xijok@gmail.com>
2023-03-28 17:37:07 +0900

    Show available language list

--- src/deepl.cr
+++ src/deepl.cr
@@ -3,12 +3,35 @@ require "./deepl/translator"
 require "./deepl/version"
 
 target_lang = "EN"
-source_lang = ""
+source_lang = "AUTO"
+
+begin
+  translator = Deepl::Translator.new
+rescue Deepl::ApiKeyError
+  STDERR.puts "ERROR: Deepl API key not found. Please set the DEEPL_API_KEY environment variable."
+  exit(1)
+end
 
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: deepl [arguments]"
-  opts.on("-f", "--from=LANG", "Source language [AUTO]") { |from| source_lang = from.upcase }
-  opts.on("-t", "--to=LANG", "Target language [EN]") { |to| target_lang = to.upcase }
+  opts.on("-f", "--from [LANG]", "Source language [AUTO]") do |from|
+    if from.empty?
+      translator.source_languages.each do |lang|
+        puts lang.values.map { |i| i.to_s }.join("\t")
+      end
+      exit
+    end
+    source_lang = from.upcase
+  end
+  opts.on("-t", "--to [LANG]", "Target language [EN]") do |to|
+    if to.empty?
+      translator.target_languages.each do |lang|
+        puts lang.values.map { |i| i.to_s }.join("\t")
+      end
+      exit
+    end
+    target_lang = to.upcase
+  end
   opts.on("-v", "--version", "Show version") do
     puts Deepl::VERSION
     exit
@@ -34,18 +57,10 @@ if input_text.empty?
 end
 
 begin
-  translator = Deepl::Translator.new
-rescue Deepl::ApiKeyError
-  STDERR.puts "ERROR: Deepl API key not found. Please set the DEEPL_API_KEY environment variable."
-  exit(1)
-end
-
-begin
   translated_text = translator.translate(input_text, target_lang, source_lang)
 rescue ex
   STDERR.puts "ERROR: #{ex}"
   exit(1)
 end
-
 
 puts translated_text
--- src/deepl/translator.cr
+++ src/deepl/translator.cr
@@ -7,7 +7,8 @@ module Deepl
   class RequestError < Exception; end
 
   class Translator
-    API_ENDPOINT = "https://api-free.deepl.com/v2/translate"
+    API_ENDPOINT           = "https://api-free.deepl.com/v2"
+    API_TRANSLATE_ENDPOINT = "#{API_ENDPOINT}/translate"
 
     @http_headers : HTTP::Headers
 
@@ -15,19 +16,15 @@ module Deepl
       @http_headers = build_http_headers
     end
 
-    def build_http_headers
+    private def build_http_headers
       HTTP::Headers{
-        "Authorization" => "DeepL-Auth-Key #{get_api_key}",
+        "Authorization" => "DeepL-Auth-Key #{api_key}",
         "Content-Type"  => "application/x-www-form-urlencoded",
       }
     end
 
-    def get_api_key
-      if ENV.has_key?("DEEPL_API_KEY")
-        ENV["DEEPL_API_KEY"]
-      else
-        raise ApiKeyError.new
-      end
+    private def api_key
+      ENV.fetch("DEEPL_API_KEY") { raise ApiKeyError.new }
     end
 
     def request_translation(text, target_lang, source_lang)
@@ -35,15 +32,15 @@ module Deepl
         "text=#{URI.encode_www_form(text)}",
         "target_lang=#{target_lang}",
       ]
-      params << "source_lang=#{source_lang}" unless source_lang.empty?
+      params << "source_lang=#{source_lang}" unless source_lang == "AUTO"
       request_payload = params.join("&")
       send_post_request(request_payload)
     end
 
-    def send_post_request(request_data)
-      HTTP::Client.post(API_ENDPOINT, body: request_data, headers: @http_headers)
-    rescue error
-      raise RequestError.new("Error: #{error} #{error.message}")
+    private def send_post_request(request_data)
+      HTTP::Client.post(API_TRANSLATE_ENDPOINT, body: request_data, headers: @http_headers)
+    rescue ex
+      raise RequestError.new("Error: #{ex.message}")
     end
 
     def translate(text, target_lang, source_lang)
@@ -54,6 +51,30 @@ module Deepl
       rescue
         raise RequestError.new("Error: #{parsed_response}")
       end
+    end
+
+    def request_languages(type)
+      HTTP::Client.get("#{API_ENDPOINT}/languages?type=#{type}", headers: @http_headers)
+    rescue ex
+      raise RequestError.new("Error: #{ex.message}")
+    end
+
+    def target_languages
+      response = request_languages("target")
+      parse_languages_response(response)
+    rescue ex
+      raise RequestError.new("Error: #{ex.message}")
+    end
+
+    def source_languages
+      response = request_languages("source")
+      parse_languages_response(response)
+    rescue ex
+      raise RequestError.new("Error: #{ex.message}")
+    end
+
+    private def parse_languages_response(response)
+      (Array(Hash(String, (String | Bool)))).from_json(response.body)
     end
   end
 end
