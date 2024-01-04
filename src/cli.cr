d026fe029d918a21a4fc03649eed4255b51395f7
kojix2 <2xijok@gmail.com>
2024-01-04 09:48:00 +0900

    Add cli.cr for testing

---
+++ src/cli.cr
@@ -0,0 +1,3 @@
+require "./deepl"
+
+Deepl.run
--- src/deepl.cr
+++ src/deepl.cr
@@ -5,38 +5,42 @@ require "./deepl/translator"
 require "./deepl/parser"
 require "./deepl/version"
 
-begin
-  translator = Deepl::Translator.new
-rescue Deepl::ApiKeyError
-  STDERR.puts "ERROR: Deepl API key not found. Please set the DEEPL_API_KEY environment variable."
-  exit(1)
-end
+module Deepl
+  def self.run
+    begin
+      translator = Deepl::Translator.new
+    rescue Deepl::ApiKeyError
+      STDERR.puts "ERROR: Deepl API key not found. Please set the DEEPL_API_KEY environment variable."
+      exit(1)
+    end
 
-parser = Deepl::Parser.new(translator)
+    parser = Deepl::Parser.new(translator)
 
-option = parser.parse(ARGV)
+    option = parser.parse(ARGV)
 
-if option.input.empty?
-  begin
-    option.input = ARGF.gets_to_end
-  rescue ex
-    STDERR.puts parser
-    STDERR.puts "ERROR: #{ex}"
-    exit(1)
+    if option.input.empty?
+      begin
+        option.input = ARGF.gets_to_end
+      rescue ex
+        STDERR.puts parser
+        STDERR.puts "ERROR: #{ex}"
+        exit(1)
+      end
+    end
+
+    spinner = Term::Spinner.new
+    translated_text = ""
+
+    begin
+      spinner = Term::Spinner.new(clear: true)
+      spinner.run do
+        translated_text = translator.translate(option)
+      end
+    rescue ex
+      STDERR.puts "ERROR: #{ex}"
+      exit(1)
+    end
+
+    puts translated_text
   end
 end
-
-spinner = Term::Spinner.new
-translated_text = ""
-
-begin
-  spinner = Term::Spinner.new(clear: true)
-  spinner.run do
-    translated_text = translator.translate(option)
-  end
-rescue ex
-  STDERR.puts "ERROR: #{ex}"
-  exit(1)
-end
-
-puts translated_text
