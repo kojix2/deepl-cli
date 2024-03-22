9c5da1fe60776bdec42aafca5700729e1cc81392
kojix2 <2xijok@gmail.com>
2024-03-22 19:47:46 +0900

    Refactor DeepL app module

--- src/deepl/app.cr
+++ src/deepl/app.cr
@@ -1,15 +1,19 @@
 module DeepL
   class App
-    getter :parser
+    getter parser : Parser
+    getter option : Options
 
     def initialize
       @parser = DeepL::Parser.new
+      @option = parser.parse(ARGV)
     end
 
     def run
-      option = parser.parse(ARGV)
-
       case option.action
+      when Action::Text
+        tlanslate_text
+      when Action::Document
+        tlanslate_document
       when Action::FromLang
         show_source_languages
       when Action::ToLang
@@ -20,8 +24,20 @@ module DeepL
         show_version
       when Action::Help
         show_help
+      else
+        raise ArgumentError.new("Invalid action: #{option.action}")
       end
+      exit(0)
+    rescue ex
+      if DeepLError.debug
+        STDERR.puts "[deepl-cli] ERROR: #{ex.class} #{ex.message}\n#{ex.backtrace.join("\n")}"
+      else
+        STDERR.puts "[deepl-cli] ERROR: #{ex.class} #{ex.message}"
+      end
+      exit(1)
+    end
 
+    def tlanslate_text
       if option.input.empty?
         option.input = ARGF.gets_to_end
       end
@@ -41,13 +57,10 @@ module DeepL
       end
 
       puts translated_text
-    rescue ex
-      if DeepLError.debug
-        STDERR.puts "[deepl-cli] ERROR: #{ex.class} #{ex.message}\n#{ex.backtrace.join("\n")}"
-      else
-        STDERR.puts "[deepl-cli] ERROR: #{ex.class} #{ex.message}"
-      end
-      exit(1)
+    end
+
+    def tlanslate_document
+      raise NotImplementedError.new("Document translation is not supported yet.")
     end
 
     def show_source_languages
@@ -56,7 +69,6 @@ module DeepL
         language, name = lang.values.map(&.to_s)
         puts "- #{language.ljust(7)}#{name}"
       end
-      exit
     end
 
     def show_target_languages
@@ -66,24 +78,20 @@ module DeepL
         formality = (supports_formality == "true") ? "YES" : "NO"
         puts "- #{language.ljust(7)}#{name.ljust(20)}\tformality support [#{formality}]"
       end
-      exit
     end
 
     def show_usage
       translator = DeepL::Translator.new
       puts translator.api_url_base
       puts translator.usage.map { |k, v| "#{k}: #{v}" }.join("\n")
-      exit
     end
 
     def show_version
       puts DeepL::VERSION
-      exit
     end
 
     def show_help
       puts parser
-      exit
     end
   end
 end
