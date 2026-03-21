3075c4c055b5c186ba1450ed94f1e9978a948392
kojix2 <2xijok@gmail.com>
2026-03-21 23:33:34 +0900

    Refactor target language test to preserve original ENV variable

--- spec/config_spec.cr
+++ spec/config_spec.cr
@@ -2,7 +2,16 @@ require "./spec_helper"
 
 describe DeepL::Config do
   it "returns the target language" do
-    ENV["DEEPL_TARGET_LANG"] = "CRYSTAL"
-    DeepL::Config.default_target_lang.should eq("CRYSTAL")
+    original = ENV["DEEPL_TARGET_LANG"]?
+    begin
+      ENV["DEEPL_TARGET_LANG"] = "CRYSTAL"
+      DeepL::Config.default_target_lang.should eq("CRYSTAL")
+    ensure
+      if original
+        ENV["DEEPL_TARGET_LANG"] = original
+      else
+        ENV.delete("DEEPL_TARGET_LANG")
+      end
+    end
   end
 end
